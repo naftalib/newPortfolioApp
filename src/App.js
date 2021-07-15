6ef79c74d86bb4c5871eda7f9301fd4cbd05b818
naftalib <45719650+naftalib@users.noreply.github.com>
2021-07-16 01:11:50 +0300

    upload#1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import About from './components/About'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
@@ -8,12 +8,15 @@ import Testimonials from './components/Testimonials'
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
+      <hr /><br />
       <Projects />
+      <hr /><br />
       <Skills />
       <Testimonials />
+      <hr /><br />
       <Contact />
     </main>
   );
